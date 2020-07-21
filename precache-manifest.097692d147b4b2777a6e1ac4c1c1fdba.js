04b1331bfde14026e2171f541f7fa097066cf43c
tnmsnhr <tnmsnhr@gmail.com>
2020-07-21 21:48:08 +0530

    Updates

---
+++ precache-manifest.097692d147b4b2777a6e1ac4c1c1fdba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0af14aa8214588fb76175ca04749bb88",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "8302338ae16cac74cc60",
+    "url": "/covid19-tracker/static/css/main.d7b15dd1.chunk.css"
+  },
+  {
+    "revision": "40e98bea70ed94571b45",
+    "url": "/covid19-tracker/static/js/2.0f1251bf.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/covid19-tracker/static/js/2.0f1251bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8302338ae16cac74cc60",
+    "url": "/covid19-tracker/static/js/main.33def33e.chunk.js"
+  },
+  {
+    "revision": "01e9f27f4ffe79c2c715",
+    "url": "/covid19-tracker/static/js/runtime-main.4a5db703.js"
+  }
+]);
